7f604ab487289b82800acffa00eeb367c04e904a
koobawuah <me@bawuahboakye.com>
2022-03-13 21:31:13 +0000

    Building Home Screen on App.js. Will be moved soon to Home.js file

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React from 'react';
 import {
   SafeAreaView,
+  View,
   StatusBar,
   StyleSheet,
   Text,
@@ -33,7 +34,21 @@ const App = (props) => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <CustomInput label='FROM'/>
+      <View style={styles.containerHeader} > 
+        <Text style={{ fontWeight: '800', fontSize: SIZES.h1 }}> NoDum </Text>
+      </View>
+      <View style={{ flex: 2, flexDirection: 'column', paddingVertical: 15, }}>
+        <View style={styles.containerTop}>
+            <CustomInput label='FROM' placeholder='MM.DD.YYYY' keyboardType='decimal-pad' />
+            <CustomInput label='To' placeholder='MM.DD.YYYY' keyboardType='decimal-pad' style={{ marginLeft: 5,}} />
+        </View>
+        <View style={{ flex: 1, paddingHorizontal: SIZES.padding }}> 
+             <CustomInput label='WHERE' placeholder='DIGITAL ADDRESS' />
+        </View>
+      </View>
+      <View style={styles.containerBody} > 
+        <Text> Product list </Text>
+      </View>
     </SafeAreaView>
   );
 };
@@ -41,9 +56,31 @@ const App = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column',
         backgroundColor: COLORS.bgColor,
         paddingHorizontal: SIZES.margin,
-    }
+    },
+    containerHeader: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        paddingHorizontal: SIZES.padding,
+        paddingBottom: 50,
+        //backgroundColor: 'red',
+    },
+    containerTop: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 30,
+        paddingHorizontal: SIZES.padding,
+        //backgroundColor: 'yellow',
+    },
+    containerBody: {
+        flex: 4,
+        //backgroundColor: 'green',
+        paddingHorizontal: SIZES.padding,
+    },
 })
 
 export default App;
